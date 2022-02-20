35bf0c8a9dd2dc85ee5e3d810f11928de44d1a7e
Valeria Caro <v.caro@uniandes.edu.co>
2022-02-19 23:03:46 -0500

    LA VIDA ES BELLA

--- Interpreter.py
+++ Interpreter.py
@@ -4,19 +4,34 @@ Proyect 0:
         The program should read a text file that 
         constains commands and definitions for the
         robot and verify whether the syntax is correct.
+
+Valeria Caro - v.caro@uniandes.edu.co - 202111040
+Sofia Velasquez - s.velasquezm2@uniandes.edu.co - 202113334
 """
 
-
-
-
-
-
-# Read the text file
-def readfile(file):
+#Read file and debug it (ignore spaces and tabs)
+def readfile (file):
+    code = []
     with open(file) as robot:
         lines = robot.readlines()
         for data in lines:
-            print(data)
+            if not data.strip():
+                continue
+            code.append(data.rstrip().lstrip())
+
+    return code
+            
+code = readfile("Robot.txt")
+print(code)
+
+# Define Grammmar
+
+command = ["defvar", "move", "=", "turn", "face", "put", "pick", "move-dir","run-dirs", "move-face", "skip"]
+
+controlStructure = ["if", "loop", "repeat", "defun"]
+
+orientations = [":north", ":south", ":east", ":west"]
 
 
-print(readfile("Robot.txt"))
+
+    
