900b9324781938e076d48a0646bc6c32a1c7a021
Valeria Caro <v.caro@uniandes.edu.co>
2022-02-22 00:03:03 -0500

    Comentario a tener en cuenta!

--- parser.py
+++ parser.py
@@ -374,7 +374,7 @@ conditions = {"facing-p": facing,
               "not": not_}
 
 code = readfile("Robot.txt")
-# Una cosita adicional, en nuestro caso en el codigo no esta funcionando el if(not(can-put-p)) y if(not(can-pick-p)
+# Una cosita adicional, en nuestro caso nuestro codigo no esta parseando el if(not(can-put-p)) y if(not(can-pick-p).
 if code != False:
    tokens = tokenizeInfo(code)
    if IsItValid(tokens):
